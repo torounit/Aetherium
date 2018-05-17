b231e30db1ac036164614701013e162e519a93f5
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-05-18 04:24:15 +0900

    remove url origin

--- inc/optimize.php
+++ inc/optimize.php
@@ -13,3 +13,24 @@ add_action( 'init', function () {
 	wp_register_script( 'jquery', false, [ 'jquery-core' ], $jquery_ver, true );
 	wp_register_script( 'jquery-core', $jquery_src, [], $jquery_ver, true );
 } );
+
+function aetherium_add_filter_remove_origin_from_uri( $hook ) {
+	add_filter( $hook, 'wp_make_link_relative' );
+}
+
+array_map( 'aetherium_add_filter_remove_origin_from_uri', [
+	"home_url",
+	"site_url",
+	"stylesheet_directory_uri",
+	"template_directory_uri",
+	"plugins_url",
+	"wp_get_attachment_url",
+	"theme_mod_header_image",
+	"theme_mod_background_image",
+	"the_content",
+	"upload_dir",
+	"widget_text",
+	"style_loader_src",
+	"script_loader_src",
+	"theme_file_uri"
+] );
