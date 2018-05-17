7175f51206e854d058c4fddcc00c9904ccdd63c1
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-05-17 16:03:03 +0900

    change theme name

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# app
+# Aetherium
 
-> A Vue.js project
+> A next-generation WordPress theme built with the Rest API and Vue.js .
 
 ## Build Setup
+
+```bash
+docker-compose -d
+```
 
 ``` bash
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
+# serve with hot reload at localhost:3000
 npm run dev
 
 # build for production with minification
--- docker/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose exec wordpress curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-docker-compose exec wordpress chmod +x wp-cli.phar
-docker-compose exec wordpress mv wp-cli.phar /usr/local/bin/wp
-
-docker-compose exec wordpress apt-get update
-docker-compose exec wordpress apt-get -y install mysql-client
-
