c28da4512233b9c8f00fb3909a3f9c610851e912
torounit <mail@torounit.com>
2018-05-17 07:09:31 +0000

    Update from travis 74b3be28395aac14717484d736933b210b3f7701

--- sw-theme.js
+++ sw-theme.js
@@ -1,7 +1,7 @@
 importScripts( 'https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js' );
 
 workbox.core.setCacheNameDetails({
-  prefix: 'vue-spa-theme',
+  prefix: 'aetherium',
   suffix: 'v1'
 });
 
