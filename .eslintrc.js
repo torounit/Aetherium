3bc47b38f9938928fb7170481699804f425a2d04
torounit <mail@torounit.com>
2018-05-21 19:19:58 +0000

    Update from travis 6ec0fe4434eafc10a1db44ec2dfb384a67031294

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  plugins: [
+    'vue'
+  ],
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  globals: {
+    'wp': true
+  }
+}
