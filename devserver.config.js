14ba42854267b6a918a62bb6ab4f8368e4502078
torounit <mail@torounit.com>
2018-05-22 08:03:51 +0000

    Update from travis 7e89c870eac9e7a61a85bd614e7fa4ce8871c842

--- devserver.config.js
+++
@@ -1,9 +0,0 @@
-const webpackConfig = require('./webpack.config')
-
-module.exports = Object.assign(webpackConfig, {
-  entry: [
-    'webpack-dev-server/client?http://localhost:' + 3000,
-    'webpack/hot/only-dev-server',
-    './src/main.js'
-  ]
-})
