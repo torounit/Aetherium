b1d96c1e591d8035e13b312c4a5f9988de02db3f
torounit <mail@torounit.com>
2018-10-28 12:32:02 +0000

    Update from travis 6fb9dc2be8eed2b2b2de1a7cad1fce733034b459

--- devserver.config.js
+++ devserver.config.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-undef */
 require( 'dotenv' ).config();
 const webpackConfig = require( './webpack.config' );
 const port = process.env.DEVSERVER_PORT;
 module.exports = Object.assign( webpackConfig, {
 	entry: [
+		'@babel/polyfill',
 		'webpack-dev-server/client?http://localhost:' + port,
 		'webpack/hot/only-dev-server',
-		'./src/main.js'
-	]
-});
+		'./src/main.js',
+	],
+} );
