4e55426e4b573fc805014c44df03dcd5a3267ad7
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-11-27 15:23:30 +0900

    fix babel config

--- devserver.config.js
+++ devserver.config.js
@@ -8,7 +8,6 @@ const port = process.env.DEVSERVER_PORT;
 
 module.exports = Object.assign( webpackConfig, {
 	entry: [
-		'@babel/polyfill',
 		'webpack-dev-server/client?http://localhost:' + port,
 		'webpack/hot/only-dev-server',
 		'./src/main.js',
