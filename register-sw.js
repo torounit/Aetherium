82d9e80b2e8a9415758b92303c70ec9f55f1494c
torounit <mail@torounit.com>
2019-11-13 11:28:47 +0000

    Update from travis d953338e3e4289615b20a857c77c58107fc6e500

---
+++ register-sw.js
@@ -0,0 +1,8 @@
+if(window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations()
+  .then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
